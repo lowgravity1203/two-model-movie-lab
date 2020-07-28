76d698d3e56bf564bb90fae2406d5ee918a3bcb3
lowgravity1203 <matthew.f.barnes1203@gmail.com>
2020-07-28 16:27:24 -0700

    encountered multiple bugs will adds delete and edit routes. evertyhing is working now

--- controllers/movies.js
+++ controllers/movies.js
@@ -6,6 +6,7 @@ const Movie = require('../models/movies')
 router.get('/', (req, res) => {
 
     Movie.find({}, (err, foundMovies) => {
+        console.log(foundMovies)
         res.render('movies/index.ejs', {
             movie: foundMovies
 
@@ -15,6 +16,42 @@ router.get('/', (req, res) => {
 
 router.get('/new', (req, res) => {
     res.render('movies/new.ejs')
+})
+
+router.get('/:id', (req, res) => {
+    Movie.findById(req.params.id, (err, foundMovies)=> {
+        res.render('movies/show.ejs', {
+            movie: foundMovies
+        })
+    })
+})
+
+router.get('/:id/edit', (req, res) => {
+    Movie.findById(req.params.id, (err, foundMovies) => {
+        res.render('movies/edit.ejs', {
+            movie: foundMovies
+        })
+    })
+})
+
+
+router.post('/', (req, res)=> {
+    Movie.create(req.body, (err, createdMovie) => {
+        
+        res.redirect('/movies')
+    })
+})
+
+router.put('/:id', (req, res) => {
+    Movie.findByIdAndUpdate(req.params.id, req.body, () => {
+        res.redirect('/movies')
+    })
+})
+
+router.delete('/:id', (req, res) => {
+    Movie.findByIdAndRemove(req.params.id, () => {
+        res.redirect('/movies')
+    })
 })
 
 module.exports = router
--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose')
 const app = express();
 const movieControllers = require('./controllers/movies.js')
+const methodOverride = require('method-override')
 
 
 const connectionString = 'mongodb://localhost/blog'
@@ -16,6 +17,9 @@ mongoose.connect(connectionString, {
 mongoose.connection.on('connected', () => console.log(`Mongoose connected to ${connectionString}`))
 mongoose.connection.on('disconnected', () => console.log('Mongoose disconnected'))
 mongoose.connection.on('error', (err) => console.log('Mongoose error', err))
+
+app.use(express.urlencoded({extended: false }))
+app.use(methodOverride('_method'))
 
 app.get('/', (req, res) => {
     res.render('index.ejs')
