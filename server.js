f62fe08866965b1a88b1136f120447afaa8ee475
bdiallo10 <bdiallo10@icloud.com>
2020-07-28 17:22:07 -0700

    encouter a bug at the server, trying to fix it

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const mongoose = require('mongoose')
 const app = express();
+const methodOverride = require('method-override')
 const movieControllers = require('./controllers/movies.js')
 const actorControllers = require('./controllers/actors.js')
-const methodOverride = require('method-override')
 
 
 const connectionString = 'mongodb://localhost/blog'
@@ -20,14 +20,14 @@ mongoose.connection.on('disconnected', () => console.log('Mongoose disconnected'
 mongoose.connection.on('error', (err) => console.log('Mongoose error', err))
 
 app.use(express.urlencoded({extended: false }))
-app.use(methodOverride('_method'))
+app.use(methodOverride('_method'));
 
 app.get('/', (req, res) => {
     res.render('index.ejs')
 })
 
 app.use('/movies', movieControllers)
-app.use('/actor', actorControllers)
+app.use('/actors', actorControllers)
 
 app.listen(3000, ()=>{
 	console.log('listening....');
